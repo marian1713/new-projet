9cffdbc5a3dc74acd3bb1668dcd413b3521eb829
marian1713 <navarromariana148@gmail.com>
2024-04-21 23:28:36 -0300

    archivos js modificado

--- JavaScrip/index.js
+++ JavaScrip/index.js
@@ -1,2 +1,36 @@
-console.log("Aqui estamos js index")
-alert("Estmoas en js")
+class Activity{
+    constructor(id,title,description,imgUrl){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    };
+};
+class Repositoy {
+    constructor(){
+        this.activities = []
+        this.id = 0
+    }
+    getAllActivities(){
+        return this.activities;
+    }
+    createActivity(title,description,imgUrl){
+        this.id++;
+        const activity = new Activity(this.id, title,description,imgUrl);
+        this.activities.push(activity);
+    };
+    deleteActivity(id){
+        this.activities = this.activities.filter(activity => activity.id !== id);
+    }
+};
+const respository = new Repositoy;
+respository.createActivity("jugar","Es lindo jugar","url");
+respository.createActivity("bailar","hace bien","url");
+respository.createActivity("salirr","distrae la mente","url");
+respository.deleteActivity(1);
+console.log(respository.getAllActivities());
+
+const alerta = function(alert) {
+    alert = ("Estamos en js");
+    return alert;
+}
